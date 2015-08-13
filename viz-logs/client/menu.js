4a4009f8de7493eb173261e16356a57f75116050
Ana Vizcarrondo <ana.vizcarrondo@gmail.com>
2015-08-13 14:43:52 +0200

    select active menu element

--- viz-logs/client/menu.js
+++ viz-logs/client/menu.js
@@ -14,5 +14,21 @@ Template.menu.events({
 	"click .underlay": function () {
 		var menu = Template.instance().find(".menu");
 		menu.classList.remove("opened");
+	},
+
+	"click .nav-item": function (e) {
+		var activeElement = e.target;
+		var menuElements = Template.instance().find("#menu").children;
+		_.each(menuElements, function (menuElement) {
+			menuElement.classList.remove("active");
+		});
+		activeElement.classList.add("active");
 	}
 });
+
+Template.menu.rendered = function() {
+	//TODO: select active element, iron router?
+	//var menuElements = Template.instance().find("#menu").children;
+	//var activeElement;
+	//activeElement.classList.add("active");
+};
