9ad0654066731e940da505fbf689679e5cca5649
Ana Vizcarrondo <ana.vizcarrondo@gmail.com>
2015-09-14 15:50:10 +0200

    mongo url again ..

--- node-server/app.js
+++ node-server/app.js
@@ -8,7 +8,7 @@ var cors = require('cors');
 //var fs = require('fs');
 
 var config = {
-	mongodbUrl: "mongodb://10.0.18.179:27017/logsDB",
+	mongodbUrl: "mongodb://lxdev01:27017/logsDB",
 	capped: true,
 	cappedSize: 1000000
 };
@@ -61,7 +61,7 @@ var nativeLogger = new (winston.Logger)({
 
 var app = express();
 //app.use(express.bodyParser());
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var corsOptions = {
 	origin: '*'
